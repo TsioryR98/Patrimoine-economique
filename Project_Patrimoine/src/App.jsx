9c35c8694873f16240379ee805eaaa9724ebbc5c
TsioryR98 <hei.tsiory.2@gmail.com>
2024-08-07 18:51:29 +0200

    feat: Adjust  App.jsx

--- Project_Patrimoine/src/App.jsx
+++ Project_Patrimoine/src/App.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import './App.css'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstr     ap/dist/css/bootstrap.min.css';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepick      er/dist/react-datepicker.css';
 
 export function App(props) {
 
@@ -49,158 +49,159 @@ const handleSubmitTrainDeVie = () => {
 //l'ajout d'une dépense
 };
 
-return (
-<>
-<div className='m-4'>
-<h1>PATRIMOINE</h1>
-</div>
-
-<div className="container">
-
-{/* CLASSE POSSESSEUR */}
-<div className="m-4 mb-3">
-<h5>POSSESSEUR</h5>
-<div className="mb-2">
-<label htmlFor="libelléTrainDeVie" className="form-label">Possesseur:</label>
-<input 
-type="text"
-id="libelléTrainDeVie"
-className="form-control"
-value={textPossesseur}
-onChange={handlePossesseurChange}
-placeholder="Nom"
-/>
-</div>
-<button className="btn btn-success" onClick={handleSubmitTrainDeVie}>Ajouter</button>
-</div>
-
-{/* CLASSE ARGENT */}
-<div className="m-4 mb-3">
-<h5>ARGENT</h5>
-<div className="mb-4">
-<DropdownButton id="dropdown-account-type" 
-title={accountType} 
-onSelect={handleAccountTypeChange}>
-<Dropdown.Item eventKey='Espèces'>Espèces</Dropdown.Item>
-<Dropdown.Item eventKey='Compte Courant'>Compte Courant</Dropdown.Item>
-<Dropdown.Item eventKey='Compte Epargne'>Compte Épargne</Dropdown.Item>
-</DropdownButton>
-</div>
-
-<div className="mb-2">
-<label htmlFor="cashAmountInput" className="form-label">Montant</label>
-<input
-type="number"
-id="cashAmountInput"
-className="form-control"
-value={cashAmount}
-onChange={handleCashAmountChange}
-placeholder="Entrez le montant"
-/>
-</div>
-
-<div className="mb-2">
-<label htmlFor="cashInterestInput" className="form-label">Intérêt</label>
-<input
-type="number"
-id="cashInterestInput"
-className="form-control"
-value={cashInterest}
-onChange={handleCashInterestChange}
-placeholder="Entrez le pourcentage"
-/>
-</div>
-
-<button className="btn btn-success" onClick={handleSubmitArgent}>Ajouter</button>
-</div>
-
-{/* CLASSE BIEN MATERIEL */}
-<div className="m-4 mb-3">
-<h5>BIENS MATERIELS</h5>
-
-<div className="mb-2">
-<label htmlFor="libelléBienMateriels" className="form-label">Libellé</label>
-<input
-type="text"
-id="libelléBienMateriels"
-className="form-control"
-value={textBiensMateriel}
-onChange={handleBienChange}
-placeholder="Le nom du bien"
-/>
-</div>
-
-<div className="mb-1">
-<label htmlFor="dateInput" className="form-label">Date mise en usage</label>
-<div className="mt-2">
-<DatePicker
-selected={acquisitionDate}
-onChange={(date) => setAcquisitionDate(date)}
-dateFormat="dd/MM/yyyy"
-className="form-control"
-/>
-</div>
-</div>
-
-<div className="mb-2">
-<label htmlFor="materialAmountInput" className="form-label">Montant</label>
-<input
-type="number"
-id="materialAmountInput"
-className="form-control"
-value={materialAmount}
-onChange={handleMaterialAmountChange}
-placeholder="Entrez le montant"
-/>
-</div>
-
-<div className="mb-2">
-<label htmlFor="depreciationRateInput" className="form-label">Taux d'amortissement</label>
-<input
-type="number"
-id="depreciationRateInput"
-className="form-control"
-value={depreciationRate}
-onChange={handleDepreciationRateChange}
-placeholder="Entrez le pourcentage"
-/>
-</div>
-
-<button className="btn btn-success" onClick={handleSubmitBienMateriel}>Ajouter</button>
-</div>
-
-{/* CLASSE TRAIN DE VIE */}
-<div className="m-4 mb-3">
-<h5>TRAIN DE VIE</h5>
-<div className="mb-2">
-<label htmlFor="libelléTrainDeVie" className="form-label">Libellé</label>
-<input
-type="text"
-id="libelléTrainDeVie"
-className="form-control"
-value={textDepense}
-onChange={handleDepenseChange}
-placeholder="Dépenses"
-/>
-</div>
-
-<div className="mb-2">
-<label htmlFor="materialAmountInput" className="form-label">Montant</label>
-<input
-type="number"
-id="materialAmountInput"
-className="form-control"
-value={materialAmount}
-onChange={handleMaterialAmountChange}
-placeholder="Entrez le montant"
-/>
-</div>
-
-<button className="btn btn-success" onClick={handleSubmitTrainDeVie}>Ajouter</button>
-</div>
-</div>
-</>
-);
-}
-
-export default App;
+          return (<>
+            
+                <div className='m-4'>
+          <h1>PATRIMOINE</h1>
+               </div>
+          
+                <div className="container">
+          
+          {/* CLASSE POSSESSEUR */}
+                <div className="m-4 mb-3">
+          <h5>POSSESSEUR</h5>
+                <div className="mb-2">
+          <label htmlFor="libelléTrainDeVie" className="form-label">Possesseur:</label>
+                <input 
+                   type="text"
+                  id="libelléTrainDeVie"
+                  className="form-control"
+                  value={textPossesseur}
+                  onChange={handlePossesseurChange}
+                  placeholder="Nom"
+                  />
+               </div>
+          <button className="btn btn-success" onClick={handleSubmitTrainDeVie}>Ajouter</button>
+               </div>
+          
+          {/* CLASSE ARGENT */}
+                <div className="m-4 mb-3">
+          <h5>ARGENT</h5>
+                <div className="mb-4">
+          <DropdownButton id="dropdown-account-type" 
+          title={accountType} 
+          onSelect={handleAccountTypeChange}>
+          <Dropdown.Item eventKey='Espèces'>Espèces</Dropdown.Item>
+          <Dropdown.Item eventKey='Compte Courant'>Compte Courant</Dropdown.Item>
+          <Dropdown.Item eventKey='Compte Epargne'>Compte Épargne</Dropdown.Item>
+          </DropdownButton>
+               </div>
+          
+                <div className="mb-2">
+          <label htmlFor="cashAmountInput" className="form-label">Montant</label>
+          <input
+          type="number"
+          id="cashAmountInput"
+          className="form-control"
+          value={cashAmount}
+          onChange={handleCashAmountChange}
+          placeholder="Entrez le montant"
+          />
+               </div>
+          
+                <div className="mb-2">
+          <label htmlFor="cashInterestInput" className="form-label">Intérêt</label>
+          <input
+          type="number"
+          id="cashInterestInput"
+          className="form-control"
+          value={cashInterest}
+          onChange={handleCashInterestChange}
+          placeholder="Entrez le pourcentage"
+          />
+               </div>
+          
+          <button className="btn btn-success" onClick={handleSubmitArgent}>Ajouter</button>
+               </div>
+          
+          {/* CLASSE BIEN MATERIEL */}
+                <div className="m-4 mb-3">
+          <h5>BIENS MATERIELS</h5>
+          
+                <div className="mb-2">
+          <label htmlFor="libelléBienMateriels" className="form-label">Libellé</label>
+          <input
+          type="text"
+          id="libelléBienMateriels"
+          className="form-control"
+          value={textBiensMateriel}
+          onChange={handleBienChange}
+          placeholder="Le nom du bien"
+          />
+               </div>
+          
+                <div className="mb-1">
+          <label htmlFor="dateInput" className="form-label">Date mise en usage</label>
+                <div className="mt-2">
+          <DatePicker
+          selected={acquisitionDate}
+          onChange={(date) => setAcquisitionDate(date)}
+          dateFormat="dd/MM/yyyy"
+          className="form-control"
+          />
+               </div>
+               </div>
+          
+                <div className="mb-2">
+          <label htmlFor="materialAmountInput" className="form-label">Montant</label>
+          <input
+          type="number"
+          id="materialAmountInput"
+          className="form-control"
+          value={materialAmount}
+          onChange={handleMaterialAmountChange}
+          placeholder="Entrez le montant"
+          />
+               </div>
+          
+                <div className="mb-2">
+          <label htmlFor="depreciationRateInput" className="form-label">Taux d'amortissement</label>
+          <input
+          type="number"
+          id="depreciationRateInput"
+          className="form-control"
+          value={depreciationRate}
+          onChange={handleDepreciationRateChange}
+          placeholder="Entrez le pourcentage"
+          />
+               </div>
+          
+          <button className="btn btn-success" onClick={handleSubmitBienMateriel}>Ajouter</button>
+               </div>
+          
+          {/* CLASSE TRAIN DE VIE */}
+                <div className="m-4 mb-3">
+          <h5>TRAIN DE VIE</h5>
+                <div className="mb-2">
+          <label htmlFor="libelléTrainDeVie" className="form-label">Libellé</label>
+          <input
+          type="text"
+          id="libelléTrainDeVie"
+          className="form-control"
+          value={textDepense}
+          onChange={handleDepenseChange}
+          placeholder="Dépenses"
+          />
+               </div>
+          
+                <div className="mb-2">
+          <label htmlFor="materialAmountInput" className="form-label">Montant</label>
+          <input
+          type="number"
+          id="materialAmountInput"
+          className="form-control"
+          value={materialAmount}
+          onChange={handleMaterialAmountChange}
+          placeholder="Entrez le montant"
+          />
+               </div>
+          
+          <button className="btn btn-success" onClick={handleSubmitTrainDeVie}>Ajouter</button>
+               </div>
+               </div>
+          </>
+          );
+          }
+          
+          export default App;
+          

18c5c64842ad885f66a89bba563bc331889df3c0
TsioryR98 <hei.tsiory.2@gmail.com>
2024-08-23 00:31:52 +0200

    feat: install expressJS for backend Server and configure patrimoineServer.js

--- Backend/patrimoineServer.js
+++ Backend/patrimoineServer.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+  res.send("Express runnig!");
 });
 
 app.listen(PORT, () => {

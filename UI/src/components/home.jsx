19231cfd025e88dc814183c1c0282883a3aa2b60
TsioryR98 <hei.tsiory.2@gmail.com>
2024-08-29 00:32:13 +0200

    build : re-install vite depedencies and config

---
+++ UI/src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+const homePage = () => {
+  return (
+    <>
+      <div className="home-page">
+        <p>BIENVENUE SUR VOTRE GESTIONNAIRE DE PATRIMOINE</p>
+      </div>
+    </>
+  );
+};
+
+export default homePage;

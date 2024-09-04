e1eae313b32ad26abef238c83d447a21f7370183
TsioryRamiadamahefa <153943203+TsioryR98@users.noreply.github.com>
2024-09-04 14:18:18 +0000

    feat: add home page

--- UI/src/components/home.jsx
+++ UI/src/components/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
-import "../css/home.css";
+import "../styles/home.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
